a6eb1f9aa3ec177fd3f3f8a9e60cdf2934df14fb
SimonGutierrez2 <135912274+SimonGutierrez2@users.noreply.github.com>
2023-11-30 22:26:45 -0300

    Add files via upload

--- trabajo final/js.js
+++ trabajo final/js.js
@@ -6,25 +6,9 @@ function enviar(){
     fecha = document.getElementById("date").value;
 
     alert("Turno cargado con exito");
-    document.write("Recuerde no olvidar estos datos para el dia de su turno");
     document.write("<br>")
-    document.write(nombre + " " + apellido)
-    document.write("<br>")
-    document.write(direccion);
-    document.write("<br>")
-    document.write(fecha); 
-}
-
-function boton(){
-    edad1 = document.getElementById("edad1").value;
-    edad2 = document.getElementById("edad2").value;
-    edad3 = document.getElementById("edad3").value;
-    suma(edad1,edad2,edad3);
-}
-
-function suma(e1,e2,e3){
-   var suma1 = parseInt(e1) + parseInt(e2) + parseInt(e3);
-   
-   alert("la suma de las edades es:" + suma1)
-}
+    document.write("Usted con el nombre de "+nombre + " " + apellido+" esta citado el dia:"+fecha+" para asistir al consultorio.")
     
+}
+
+   
